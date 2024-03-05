f6ac0b98b8569a542d22870d207d433ab3bb9502
smengual <sebastian.mengual@UGent.be>
2024-03-05 11:26:15 +0100

    SS model with modflow 6 works, now comes the calibration

--- Modflow_SS/mod_utils_SS.py
+++ Modflow_SS/mod_utils_SS.py
@@ -12,6 +12,8 @@ from shapely.geometry import Polygon, Point, LineString
 import rasterio
 from rasterio.features import rasterize
 import geopandas as gpd
+from flopy.mf6.modflow.mfgwfnpf import ModflowGwfnpf
+from flopy.mf6.modflow.mfgwfsto import ModflowGwfsto
 pd.options.mode.chained_assignment = None  # default='warn'
 
 
@@ -98,7 +100,107 @@ def set_hydraulic_properties(sim, gdf, s, bbox, delr, delc, hk_attributes, vk_at
         # Set the specific storage in this layer of the MODFLOW model
         sim.lpf.ss[i] = ss_raster
 
+from flopy.mf6.modflow.mfgwfnpf import ModflowGwfnpf
 
+from flopy.mf6.modflow.mfgwfnpf import ModflowGwfnpf
+from flopy.mf6.modflow.mfgwfsto import ModflowGwfsto
+
+def set_hydraulic_properties_mf6(sim, gwf, gdf, s, bbox, delr, delc, hk_attributes, vk_attributes, sy_attributes, ss_attributes):
+    # Define the spatial resolution of your MODFLOW grid
+    xres = delr
+    yres = delc
+
+    # Define the bounds of your MODFLOW grid
+    xmin = int((bbox[0])//100*100)
+    ymin = int((bbox[1])//100*100+100)
+    xmax = int((bbox[2])//100*100)
+    ymax = int((bbox[3])//100*100+100)
+
+    # Create an empty raster dataset with the same spatial resolution and bounds as your MODFLOW grid
+    transform = rasterio.transform.from_origin(xmin, ymax, xres, yres)
+    out_shape = (int((ymax-ymin)/yres), int((xmax-xmin)/xres))
+
+    # Get the number of layers, rows, and columns
+    nlay, nrow, ncol = gwf.dis.nlay.array, gwf.dis.nrow.array, gwf.dis.ncol.array
+
+    # Initialize k, k33, sy, and ss as arrays
+    k_array = np.ones((nlay, nrow, ncol))
+    k33_array = np.ones((nlay, nrow, ncol))
+    sy_array = np.ones((nlay, nrow, ncol))
+    ss_array = np.ones((nlay, nrow, ncol))
+
+    # Loop over each layer
+    for i in range(len(hk_attributes)):
+        # Check if there are any non-zero values in the shapefile for this attribute
+        if any(gdf[hk_attributes[i]] != 0):
+            # Rasterize the horizontal hydraulic conductivity attribute of the shapefile for this layer
+            hk_raster = rasterize(
+                ((geom,value) for geom, value in zip(gdf.geometry, gdf[hk_attributes[i]]) if value != 0),
+                out_shape=out_shape,
+                transform=transform,
+                fill=1,  # use 1 as the fill value
+                default_value=1,
+                dtype='float64'
+            )
+        else:
+            # If all values are 0, create a raster filled with the default value
+            hk_raster = np.full(out_shape, 1, dtype='float64')
+
+        # Set the horizontal hydraulic conductivity in this layer of the MODFLOW model
+        k_array[i] = hk_raster
+
+        # Repeat similar steps for 'vka', 'sy', and 'ss'
+        if any(gdf[vk_attributes[i]] != 0):
+            vk_raster = rasterize(
+                ((geom,value) for geom, value in zip(gdf.geometry, gdf[vk_attributes[i]]) if value != 0),
+                out_shape=out_shape,
+                transform=transform,
+                fill=1,
+                default_value=0.1,
+                dtype='float64'
+            )
+        else:
+            vk_raster = np.full(out_shape, 0.1, dtype='float64')
+
+        k33_array[i] = vk_raster
+
+        if any(s[sy_attributes[i]] != 0):
+            sy_raster = rasterize(
+                ((geom,value) for geom, value in zip(s.geometry, s[sy_attributes[i]]) if value != 0),
+                out_shape=out_shape,
+                transform=transform,
+                fill=1,
+                default_value=0.05,
+                dtype='float64'
+            )
+        else:
+            sy_raster = np.full(out_shape, 0.05, dtype='float64')
+
+        sy_array[i] = sy_raster
+
+        if any(s[ss_attributes[i]] != 0):
+            ss_raster = rasterize(
+                ((geom,value) for geom, value in zip(s.geometry, s[ss_attributes[i]]) if value != 0),
+                out_shape=out_shape,
+                transform=transform,
+                fill=1,
+                default_value=0.001,
+                dtype='float64'
+            )
+        else:
+            ss_raster = np.full(out_shape, 0.001, dtype='float64')
+
+        # Check if any values in the raster exceed the threshold
+        ss_raster = np.where(ss_raster > 0.009, 0.009, ss_raster)
+
+        # Set the specific storage in this layer of the MODFLOW model
+        ss_array[i] = ss_raster
+
+    # Create a new npf package with the k and k33 values
+    npf = ModflowGwfnpf(gwf, k=k_array, k33=k33_array)
+
+    # Create a new sto package with the sy and ss values
+    sto = ModflowGwfsto(gwf, sy=sy_array, ss=ss_array, steady_state={0: True}, save_flows=False)
 
 def interpolate_to_grid(file_list,xul,yul,nrow,ncol,delr,delc,skiprows=None):
     """interpolate asc data to a model grid using natural neighbor
